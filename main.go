36bc04bcc67de2f5a78c968de219567a44aa0edb
fanwei <zxr615@foxmail.com>
2021-10-14 23:39:28 +0800

    init

---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go-image-water-case/pkg"
+	"log"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var num int
+var concurrent int
+var sweep bool
+
+func main() {
+	flag.IntVar(&num, "n", 50, "图片数量")
+	flag.IntVar(&concurrent, "c", 1, "并发数量")
+	flag.BoolVar(&sweep, "s", false, "清理生成出来的水印")
+	flag.Parse()
+
+	fmt.Printf("水印图：%d 张\n", num)
+
+	syncGen()
+	goroutineGen()
+	goroutineCtrlGen()
+}
+
+// 同步
+func syncGen() {
+	startT := time.Now()
+
+	// 打开水印图
+	water, err := pkg.OpenPngImage("./test_water.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 这个循环是遍历原图
+	imgPath := "./test.png"
+	for i := 1; i <= num; i++ {
+		id := i + 1000 // 模拟 id
+		if err = pkg.Generate(imgPath, id, water); err != nil {
+			log.Println("水印图生成失败，id=" + strconv.Itoa(i))
+		}
+	}
+
+	// 计算耗时
+	tc := time.Since(startT)
+
+	fmt.Printf("同步执行时间 = %v\n", tc)
+}
+
+// 无限制并发
+func goroutineGen() {
+	startT := time.Now()
+
+	water, err := pkg.OpenPngImage("./test_water.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	imgPath := "./test.png"
+
+	wg := sync.WaitGroup{}
+
+	// 这个循环是遍历目录下的原图
+	for i := 1; i <= num; i++ {
+		wg.Add(1)
+		id := i + 1000 // 模拟 id
+		go func() {
+			defer wg.Done()
+			if err = pkg.Generate(imgPath, id, water); err != nil {
+				log.Println("水印图生成失败，id=" + strconv.Itoa(i))
+			}
+		}()
+	}
+
+	wg.Wait()
+	// 计算耗时
+	tc := time.Since(startT)
+	fmt.Printf("无并发数量控制时间 = %v\n", tc)
+}
+
+// 控制并发数量
+func goroutineCtrlGen() {
+	startT := time.Now()
+	wg := sync.WaitGroup{}
+	// 指定缓冲数量
+	ch := make(chan struct{}, 2)
+
+	water, err := pkg.OpenPngImage("./test_water.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 模拟数据量
+	// 这个循环是遍历目录下的原图
+	for i := 1; i <= num; i++ {
+		// 模拟读取的量
+		imgPath := "./test.png"
+		wg.Add(1)
+
+		id := i + 1000 // 模拟 id
+		go func() {
+			defer wg.Done()
+			ch <- struct{}{}
+
+			if err = pkg.Generate(imgPath, id, water); err != nil {
+				log.Println("水印图生成失败，id=" + strconv.Itoa(id))
+			}
+			<-ch
+		}()
+	}
+
+	wg.Wait()
+	// 计算耗时
+	tc := time.Since(startT)
+	fmt.Printf("同时并发 %d 个时间 = %v\n", concurrent, tc)
+}
---
+++ pkg/util.go
@@ -0,0 +1,121 @@
+package pkg
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"image/jpeg"
+	"image/png"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/golang/freetype"
+	"github.com/golang/freetype/truetype"
+)
+
+// Generate 生成水印
+// imgPath 源文件路径
+// id 文字水印id
+// waterImg 水印
+func Generate(imgPath string, id int, water image.Image) error {
+	// 打开原图文件
+	origin, err := OpenPngImage(imgPath)
+	if err != nil {
+		return err
+	}
+
+	x := origin.Bounds().Dx() // 图片长(px)
+	y := origin.Bounds().Dy() // 图片宽(px)
+
+	// 水印位置：以原图长宽 - 去水印图 - 偏移像素
+	waterPosition := image.Pt(
+		x-water.Bounds().Dx()-100,
+		y-water.Bounds().Dy()-100,
+	)
+
+	// 新建一个画布，把原图、水印、文字叠加上去
+	newImage := image.NewRGBA(origin.Bounds())
+	// 叠加原图
+	draw.Draw(newImage, origin.Bounds(), origin, image.Point{}, draw.Over)
+	// 叠加水印
+	draw.Draw(newImage, water.Bounds().Add(waterPosition), water, image.Point{}, draw.Over)
+
+	// 加载字体
+	font, err := GetFont()
+	if err != nil {
+		return err
+	}
+	fc := freetype.NewContext()   // xx
+	fc.SetDPI(72)                 // dpi
+	fc.SetFont(font)              // 字体
+	fc.SetFontSize(48)            // 字体大小(磅)
+	fc.SetClip(newImage.Bounds()) // 设置剪裁矩形以进行绘制
+	fc.SetDst(newImage)           // 设置目标图像(字体加在哪里)
+	//fc.SetSrc(image.Black)        // 字体颜色
+	// 可根据 RGBA 设置颜色
+	fc.SetSrc(image.NewUniform(color.RGBA{R: 21, G: 33, B: 57, A: 255}))
+
+	// 绘制文字eg: id:10086
+	text := "id: " + strconv.Itoa(id)
+	// 字体水印位置：在水印位置的基础上 偏移 Y 轴
+	fontPt := freetype.Pt(waterPosition.X, waterPosition.Y+200)
+	if _, err = fc.DrawString(text, fontPt); err != nil {
+		return err
+	}
+
+	// 创建水印文件
+	savePath := "./water/" + strconv.Itoa(id) + ".png"
+	created, err := os.Create(savePath)
+	if err != nil {
+		return err
+	}
+	defer created.Close()
+
+	// 写入
+	if err = jpeg.Encode(created, newImage, &jpeg.Options{Quality: 100}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// OpenPngImage 获取水印
+func OpenPngImage(path string) (image.Image, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	water, err := png.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+
+	return water, nil
+}
+
+// GetFont 获取字体
+func GetFont() (*truetype.Font, error) {
+	fontBytes, err := ioutil.ReadFile("./font.ttf")
+	if err != nil {
+		return nil, err
+	}
+
+	return freetype.ParseFont(fontBytes)
+}
+
+// Go 避免 go func(){} 如果方法中 抛出 panic 无法被捕获到
+// 或者是每在每个 go 前面都 recover() 一次，造成的代码混乱不可维护
+func Go(f func()) {
+	defer func() {
+		if err := recover(); err != nil {
+			// 记录日志
+			log.Println(err)
+		}
+	}()
+
+	go f()
+}
